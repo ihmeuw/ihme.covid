8d1667612cfb07e18cc247072159666e18415a66
Mike Richards <miker985@uw.edu>
2020-05-26 08:34:25 -0700

    Add second option to specify default model-inputs version

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,8 @@
   # http://r-pkgs.had.co.nz/r.html#r-differences
   op <- options()
   op.ihme.covid <- list(
-    "ihme.covid.roots.model-inputs" = "/ihme/covid-19/model-inputs"
+    "ihme.covid.roots.model-inputs" = "/ihme/covid-19/model-inputs",
+    "ihme.covid.versions.model-inputs" = "best"
   )
   toset <- !(names(op.ihme.covid) %in% names(op))
   if (any(toset)) {
--- tests/testthat/test_zzz.R
+++ tests/testthat/test_zzz.R
@@ -1,6 +1,11 @@
 test_that("zzz.R .onLoad sets required defaults", {
-  default.model.inputs <- getOption("ihme.covid.roots.model-inputs")
+  default.model.inputs.path <- getOption("ihme.covid.roots.model-inputs")
 
-  expect_false(is.null(default.model.inputs))
-  expect_equal(default.model.inputs, "/ihme/covid-19/model-inputs")
+  expect_false(is.null(default.model.inputs.path))
+  expect_equal(default.model.inputs.path, "/ihme/covid-19/model-inputs")
+
+
+  default.model.inputs.version <- getOption("ihme.covid.versions.model-inputs")
+  expect_false(is.null(default.model.inputs.version))
+  expect_equal(default.model.inputs.version, "best")
 })
