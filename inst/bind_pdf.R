6587790acda200fd45f24373f866837bb29ec6eb
Emily Linebarger <elineb@gen-uge-submit-p01.hosts.ihme.washington.edu>
2020-06-22 14:12:31 -0700

    Working out bugs in ordering PDFs

--- inst/bind_pdf.R
+++ inst/bind_pdf.R
@@ -1,22 +1,22 @@
-#' Binds together the PDFs from an array job - is called by 'pdf_array_job()'. 
+#' Binds together the PDFs from an array job - is called by 'pdf_array_job()'.
 #'
 bind_pdfs = function(temp_out_dir, final_out_dir, write_file_name){
   files = list.files(temp_out_dir, full.names=T, include.dirs=F)
   
-  # Add zeros to job ID so they will sort in the order of jobs file 
-  files = gsub(paste0(temp_out_dir, "/"), "", files)
-  files = gsub(".png", "", files)
-  files[nchar(files)==1] = paste0("0", files[nchar(files)==1])
-  files = sort(files)
+  # Add zeros to job ID so they will sort in the order of jobs file
+  order = data.table::data.table(file = files)
+  order[, order:=gsub(paste0(temp_out_dir, "/"), "", files)]
+  order[, order:=gsub(".png", "", order)]
+  order[nchar(order)==1, order:=paste0("0", order)]
+  order = order[order(order)]
   
   pdf(paste0(final_out_dir, "/", write_file_name))
-  for (file in files){
-    file = paste0(temp_out_dir, "/", file, ".png")
+  for (file in order$file){
     grob = grid::rasterGrob(png::readPNG(file, native = FALSE), interpolate = FALSE)
     gridExtra::grid.arrange(grob)
-  } 
+  }
   dev.off()
-} 
+}
 
 parser <- argparse::ArgumentParser()
 parser$add_argument("--temp-out-dir", required=TRUE, help="Temporary directory where files are saved")
