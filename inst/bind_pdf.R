d3186120c33e5ba2855a8f03d213eecba2168662
Emily Linebarger <elineb@gen-uge-submit-p01.hosts.ihme.washington.edu>
2020-06-23 20:58:24 -0700

    Responding to PR comments

--- inst/bind_pdf.R
+++ inst/bind_pdf.R
@@ -5,12 +5,9 @@ bind_pdfs = function(temp_out_dir, final_out_dir, write_file_name){
   
   # Add zeros to job ID so they will sort in the order of jobs file
   order = data.table::data.table(file = files)
-  order[, order:=gsub(paste0(temp_out_dir, "/"), "", files)]
-  order[, order:=gsub(".png", "", order)]
-  order[nchar(order)==1, order:=paste0("000", order)]
-  order[nchar(order)==2, order:=paste0("00", order)]
-  order[nchar(order)==3, order:=paste0("0", order)]
-  order = order[order(order)]
+  order[, job_id:=gsub(paste0(temp_out_dir, "/"), "", files)]
+  order[, job_id:=gsub(".png", "", job_id)]
+  order = order[order(as.numeric(job_id))]
   
   pdf(paste0(final_out_dir, "/", write_file_name))
   for (file in order$file){
