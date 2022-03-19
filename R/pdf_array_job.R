8dee92a395420eb62995dac380818f9b7c39dbea
Emily Linebarger <elineb@uw.edu>
2022-03-18 17:01:19 -0700

    Transitioning array job to work on slurm

--- R/pdf_array_job.R
+++ R/pdf_array_job.R
@@ -4,21 +4,21 @@
 #' @param r_script Full path to the R script that has your plotting code.
 #' @param final_out_dir Path to directory to save final file. Needs to be to a path on the cluster. 
 #' @param write_file_name Final file name to write. Defaults to "all_jobs.pdf". 
-#' @param rshell R-shell to use. Defaults to LBD shell. 
+#' @param rshell R-shell to use. Defaults to SciComp shell. 
 #' @param fthread fthreads for cluster jobs. Defaults to 5. 
 #' @param fmem fmem for cluster jobs. Defaults to 10G. 
 #' @param h_rt h_rt for cluster jobs. Defaults to 2 hours. 
 #' @param queue Queue for cluster jobs. Options are 'all.q', 'i.q', 'long.q', or 'd.q'. Defaults to 'all.q'. 
 #' @param project Cluster project. Defaults to 'proj_covid'. 
 #' @param job_name Cluster job name. Defaults to 'pdf_array'. 
-#' @param output Path to write cluster output messages to. Defaults to "/share/temp/sgeoutput/USERNAME/output"
-#' @param errors Path to write cluster error messages to. Defaults to "/share/temp/sgeoutput/USERNAME/errors"
+#' @param output Path to write cluster output messages to. Defaults to "/ihme/temp/slurmoutput/USERNAME/output"
+#' @param errors Path to write cluster error messages to. Defaults to "/ihme/temp/slurmoutput/USERNAME/errors"
 #' @param remove_temp_files Remove temporary files after jobs have completed? Defaults to TRUE.
 #' @param args Additional arguments to pass to array job launch script. 
 pdf_array_job <- function(jobs_file, r_script, final_out_dir, 
                           write_file_name="all_jobs.pdf",
-                          rshell="/share/singularity-images/lbd/shells/singR.sh -e s", 
-                          fthread=5, fmem='10G', h_rt="00:02:00:00", queue="all.q",
+                          rshell="/ihme/singularity-images/rstudio/shells/execRscript.sh -s", 
+                          fthread=5, fmem='10G', h_rt="02:00:00", queue="all.q",
                           project="proj_covid", job_name="pdf_array", output=NULL, errors=NULL, 
                           remove_temp_files=TRUE, args=NULL){
   # Global variables 
@@ -26,10 +26,10 @@ pdf_array_job <- function(jobs_file, r_script, final_out_dir,
   uuid = uuid::UUIDgenerate()
   
   # Set up output/error logging
-  if (is.null(output)) output=paste0("/share/temp/sgeoutput/", user, "/output")
+  if (is.null(output)) output=paste0("/ihme/temp/slurmoutput/", user, "/output")
   output = paste0(output, "/", uuid)
   if (!dir.exists(output)) dir.create(output, recursive=TRUE)
-  if (is.null(errors)) errors=paste0("/share/temp/sgeoutput/", user, "/errors")
+  if (is.null(errors)) errors=paste0("/ihme/temp/slurmoutput/", user, "/errors")
   errors = paste0(errors, "/", uuid)
   if (!dir.exists(errors)) dir.create(errors, recursive=TRUE)
   
@@ -42,15 +42,16 @@ pdf_array_job <- function(jobs_file, r_script, final_out_dir,
   
   jobs = data.table::fread(jobs_file)
   command <- paste0(
-    "qsub", 
-    " -l fthread=", fthread, ",fmem=", fmem, ",h_rt=", h_rt, 
-    " -t 1:", nrow(jobs), 
-    " -q ", queue, 
-    " -P ", project, 
-    " -N ", job_name, 
-    " -now no",
-    " -o ", output, 
-    " -e ", errors,
+    "sbatch", 
+    " --mem ", fmem,
+    " -c ", fthread, 
+    " -t ", h_rt, 
+    " --array=1-", nrow(jobs), 
+    " -p ", queue, 
+    " -A ", project, 
+    " -J ", job_name, 
+    " -o ", output, "/%x.o%j",
+    " -e ", errors, "/%x.e%j",
     " ", rshell, 
     " ", r_script,
     " --jobs-file ", jobs_file, 
@@ -64,21 +65,23 @@ pdf_array_job <- function(jobs_file, r_script, final_out_dir,
   }
   print("Running array jobs command:")
   print(command)
-  system(command)
+  job_id = system(command, intern = TRUE)
+  job_id = gsub("[[:alpha:]]| ", "", job_id)
   
   # Once jobs have finished, bind them back together
   bind_job_name = paste0("bind_", job_name)
   bind_script = system.file("bind_pdf.R", package="ihme.covid", lib.loc="/ihme/covid-19/.r-packages/current", mustWork=TRUE)
   command <- paste0(
-    "qsub", 
-    " -l fthread=", fthread, ",fmem=", fmem, ",h_rt=", h_rt, ",archive=TRUE",
-    " -q ", queue, 
-    " -P ", project, 
-    " -N ", bind_job_name, 
-    " -now no",
-    " -hold_jid ", job_name,
-    " -o ", output, 
-    " -e ", errors,
+    "sbatch", 
+    " --mem ", fmem,
+    " -c ", fthread, 
+    " -t ", h_rt, 
+    " -p ", queue, 
+    " -A ", project, 
+    " -J ", bind_job_name, 
+    " -o ", output, "/%x.o%j",
+    " -e ", errors, "/%x.e%j",
+    " --dependency ", job_id,
     " ", rshell, 
     " ", bind_script, 
     " --temp-out-dir ", temp_out_dir, 
@@ -87,7 +90,8 @@ pdf_array_job <- function(jobs_file, r_script, final_out_dir,
   )
   print("Running PDF binding command:")
   print(command)
-  system(command)
+  job_id = system(command, intern = TRUE)
+  job_id = gsub("[[:alpha:]]| ", "", job_id)
   
   # Remove temporary directories and their contents.
   # All have been created within this function with a UUID,
@@ -96,15 +100,14 @@ pdf_array_job <- function(jobs_file, r_script, final_out_dir,
   if(remove_temp_files==TRUE){
     for(i in 1:length(temp_file_dirs)){
       command <- paste0(
-        "qsub",
-        " -l fthread=2,fmem=10G,h_rt=00:00:45:00,archive=TRUE",
-        " -q ", queue,
-        " -P ", project,
-        " -N clean_temp_files_", i,
-        " -now no",
-        " -hold_jid ", bind_job_name,
-        " -b yes", 
-        " rm -r ", temp_file_dirs[i]
+        "sbatch",
+        " --mem 10G -c 2 -t 00:45:00 -C archive",
+        " -p ", queue,
+        " -A ", project,
+        " -J clean_temp_files_", i,
+        " --dependency ", job_id,
+        " --wrap", 
+        " 'rm -r ", temp_file_dirs[i], "'"
       )
       system(command)
     }
