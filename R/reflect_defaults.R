152e14d6c4e86ef8e62d8ec9d129a70128620ece
Emily Linebarger <elineb@uw.edu>
2022-05-23 13:41:43 -0700

    Wrapping entire reflect_defaults in tryCatch to see if that helps

--- R/reflect_defaults.R
+++ R/reflect_defaults.R
@@ -4,25 +4,29 @@ reflect_defaults <- function() {
 }
 
 .reflect.locations <- function(root = getOption("ihme.covid.roots.model-inputs"), version = getOption("ihme.covid.versions.model-inputs")) {
-  path <- file.path(root, version, "locations", "ids.yaml")
-  if (file.exists(path)) {
-    obj <- yaml::yaml.load_file(path)
-
-    covariate <- obj$covariate
-    if (is.null(covariate)) {
-      warning(sprintf("No 'covariate' key in %s", path))
+  tryCatch({
+    path <- file.path(root, version, "locations", "ids.yaml")
+    if (file.exists(path)) {
+      obj <- yaml::yaml.load_file(path)
+  
+      covariate <- obj$covariate
+      if (is.null(covariate)) {
+        warning(sprintf("No 'covariate' key in %s", path))
+      } else {
+        options("ihme.covid.locations.covariate" = covariate)
+      }
+  
+      modeling <- obj$modeling
+      if (is.null(modeling)) {
+        warning(sprintf("No 'modeling' key in %s", path))
+      } else {
+        options("ihme.covid.locations.modeling" = modeling)
+      }
+  
     } else {
-      options("ihme.covid.locations.covariate" = covariate)
+      warning(sprintf("Warning: could not automatically determine covariate/modeling location set versions - %s does not exist", path))
     }
-
-    modeling <- obj$modeling
-    if (is.null(modeling)) {
-      warning(sprintf("No 'modeling' key in %s", path))
-    } else {
-      options("ihme.covid.locations.modeling" = modeling)
-    }
-
-  } else {
-    warning(sprintf("Warning: could not automatically determine covariate/modeling location set versions - %s does not exist", path))
-  }
+  }, error = function(cond){ # TODO: This seems to be happening when running with Github CI. To revisit. 
+    warning(sprintf("Other error was thrown: %s", cond))
+  })
 }
