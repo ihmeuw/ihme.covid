6e8aadb6a7a86eace5216bb049731a92f91095b3
irleonardo <leonardo@irl.com>
2022-09-16 10:26:49 -0500

    test commit

--- R/children_of_parents.R
+++ R/children_of_parents.R
@@ -14,7 +14,7 @@
 ##'  \item{\code{"boolean"} : T/F vector for all rows in the hierarchy - useful for adding a new column}
 ##'  \item{\code{"numeric"} : vector of only the childrens' location_ids (and parent, if desired)}
 ##' }
-#' @return Logical T/F vector for all hierarchy locs, or a vector of children location_ids?
+#' @return Logical T/F vector for all hierarchy locs, or a vector of children location_ids.
 #' @export
 #'
 #' @examples
@@ -27,6 +27,7 @@
 #'
 #' child_locs_of_china <- children_of_parents(6, hierarchy, output = "loc_ids")
 #'
+
 children_of_parents <- function(
   parent_loc_ids, # vector of parent location_ids
   hierarchy, # which hierarchy?
@@ -34,55 +35,55 @@ children_of_parents <- function(
   include_parent = FALSE # include parent with children, or only children?
 ){
   validate_children_of_parents_inputs(output, hierarchy)
-  
+
   result_vec = c()
   for (i in 1:nrow(hierarchy)){
     parent_check_results = c()
     for (parent_id in parent_loc_ids){
-      # There is an off-by-one error here. 
+      # There is an off-by-one error here.
       # the current location ID is included in the path_to_top_parent,
       # so to get behavior right for "include_parent", negate the check when location_id == parent_id.
       if (include_parent){
-        check = is_child_of_parent(parent_id, hierarchy$path_to_top_parent[i]) 
+        check = is_child_of_parent(parent_id, hierarchy$path_to_top_parent[i])
       } else {
-        check = is_child_of_parent(parent_id, hierarchy$path_to_top_parent[i]) & 
+        check = is_child_of_parent(parent_id, hierarchy$path_to_top_parent[i]) &
           hierarchy$location_id[i] != parent_id
       }
       parent_check_results = c(parent_check_results, check)
     }
     result_vec = c(result_vec, any(parent_check_results))
   }
-  
+
   stopifnot(length(result_vec) == nrow(hierarchy))
-  
+
   if (output == "loc_ids"){
     return(unique(hierarchy[result_vec, location_id]))
   }
   return(result_vec) # Only other option is "boolean"
-  
+
 }
 
-#' @description Helper function for children_of_parents. 
+#' @description Helper function for children_of_parents.
 validate_children_of_parents_inputs = function(output, hierarchy){
   # check for valid method
   output_options <- c("boolean", "loc_ids")
   if (!(output %in% output_options)) {
     stop("Invalid output argument, please choose: boolean, loc_ids")
   }
-  
+
   # Check for valid hierarchy
   if (!all(c('path_to_top_parent', 'location_id') %in% names(hierarchy))){
     stop("Was passed an invalid hierarchy. Must have columns path_to_top_parent and location_id.")
   }
 }
 
-#' @description Helper function for children_of_parents. 
-#' Given a single parent_id and a path_to_top_parent, 
-#' returns TRUE if that parent_id is in the path. 
-#' 
+#' @description Helper function for children_of_parents.
+#' Given a single parent_id and a path_to_top_parent,
+#' returns TRUE if that parent_id is in the path.
+#'
 #' @param parent_id [int] Location ID of parent to test
 #' @param path_to_top_parent [character] String of path to top parent from hierarchy
-#' 
+#'
 #' @return boolean
 is_child_of_parent = function(parent_id, path_to_top_parent){
   path_to_top_parent = as.integer(unlist(strsplit(path_to_top_parent, ",")))
