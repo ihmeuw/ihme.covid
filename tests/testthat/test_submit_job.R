6b6955e70c6b17bb688c4d4f22b34f8b64571e5d
Kaleb Coberly <kalcob43@gen-slurm-slogin-p02.cluster.ihme.washington.edu>
2022-09-28 13:48:03 -0700

    List first tests.

---
+++ tests/testthat/test_submit_job.R
@@ -0,0 +1,9 @@
+
+# Return must be valid.
+## Return type must be (?).
+## A valid test script must return a job id.
+
+# Inputs must be valid.
+## script_path must be a string.
+## script_path must be a non-empty string.
+## script_path must be a valid filepath.
