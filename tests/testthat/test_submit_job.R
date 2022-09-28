860e125f642aadcf3f20a4e7d26ba5edf0b9980b
Kaleb Coberly <kalcob43@gen-slurm-slogin-p02.cluster.ihme.washington.edu>
2022-09-28 15:51:41 -0700

    Set script1 path.

--- tests/testthat/test_submit_job.R
+++ tests/testthat/test_submit_job.R
@@ -1,18 +1,18 @@
 
 # NOTE: Set your working directory to the repo.
 test_scripts_dir = "tests/testthat/fixtures/sbatch_scripts"
+script1 = script_path = file.path(test_scripts_dir, "test_script_1.R")
 return_regex = "Submitted batch job \\d{8}"
 
 # Must run with just a valid script path.
 test_that(
   desc = "Must run with just a valid script path.",
   {
-    script_path = file.path(test_scripts_dir, "test_script_1.R")
-    result = submit_job(script_path = script_path)
+    result = submit_job(script_path = script1)
     expect(
       ok = grepl(return_regex, result),
       failure_message = paste0(
-        "Passing only script_path ", script_path,
+        "Passing only script_path ", script1,
         " produced invalid submission return: ", result
       )
     )
@@ -23,12 +23,11 @@ test_that(
 test_that(
   desc = "Return must be 'Submitted batch job <job id>'.",
   {
-    script_path = file.path(test_scripts_dir, "test_script_1.R")
-    result = submit_job(script_path = script_path)
+    result = submit_job(script_path = script1)
     expect(
       ok = grepl(return_regex, result),
       failure_message = paste0(
-        "Passing only script_path ", script_path,
+        "Passing only script_path ", script1,
         " produced invalid submission return: ", result
       )
     )
@@ -66,28 +65,32 @@ test_that(
 test_that(
   desc = "Command must be valid.",
   {
+    # Arguments are grouped with the commands/scripts to which they belong.
+    ## sbatch args follow sbatch and precede shell_path.
+    ## shell args follow shall_path and precede script_path.
+    ## script args follow script_path.
+    
     # script_path is in the right place in command.
-    script_path = file.path(test_scripts_dir, "test_script_1.R")
     ## script_path is after a shell script with a -s flag.
     expect_message(
-      object = submit_job(script_path = script_path, see_command = T),
-      regexp = paste0("command: sbatch .*[.]sh .*-s ", script_path)
+      object = submit_job(script_path = script1, see_command = T),
+      regexp = paste0("command: sbatch .*[.]sh .*-s ", script1)
     )
     ## script_path directly follows image_path when it is passed.
     image_path = "/ihme/singularity-images/rstudio/ihme_rstudio_3631.img"
     expect_message(
       object = submit_job(
-        script_path = script_path,
+        script_path = script1,
         image_path = image_path,
         see_command = T
       ),
-      regexp = paste0("command: sbatch .* -i ", image_path, " -s ", script_path)
+      regexp = paste0("command: sbatch .* -i ", image_path, " -s ", script1)
     )
     
     # image_path directly follows shell script when passed.
     expect_message(
       object = submit_job(
-        script_path = script_path,
+        script_path = script1,
         image_path = image_path,
         see_command = T
       ),
@@ -95,9 +98,7 @@ test_that(
     )
   }
 )
-## sbatch args follow sbatch and precede shell_path.
-## shell args follow shall_path and precede script_path.
-## script args follow script_path.
+
 ## job_name is in command (set and passed).
 ## mem is in command.
 ## archive is in command.
