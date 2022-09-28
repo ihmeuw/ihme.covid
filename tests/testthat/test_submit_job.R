e2c430863b9fcf86431d87e280e9dc57d8480e4f
Kaleb Coberly <kalcob43@gen-slurm-slogin-p02.cluster.ihme.washington.edu>
2022-09-28 14:00:50 -0700

    List more tets conditions.

--- tests/testthat/test_submit_job.R
+++ tests/testthat/test_submit_job.R
@@ -7,3 +7,21 @@
 ## script_path must be a string.
 ## script_path must be a non-empty string.
 ## script_path must be a valid filepath.
+
+# Command must be valid.
+## script_path is in command.
+## job_name is in command (set and passed).
+## mem is in command.
+## archive is in command.
+## cores is in command.
+## runtime is in command.
+## partition is in command.
+## account is in command.
+## output_path is in command.
+## error_path is in command when passed.
+## sbatch args are in command when passed.
+## shell_path is in command.
+## image_path is in command when passed.
+## shell args are in command when passed.
+## script args are in command when passed.
+## command output when see_command set to true.
