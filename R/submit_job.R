845726587b25e709ad4b675ba1091c6d4e43efac
Kaleb Coberly <kalcob43@gen-slurm-slogin-p02.cluster.ihme.washington.edu>
2022-09-28 09:19:47 -0700

    Add documentation.

--- R/submit_job.R
+++ R/submit_job.R
@@ -1,18 +1,40 @@
-# TODO: write docstring
+# TODO: Find best way to document defaults.
+# TODO: How to explicitly allow multiple argument data types? Does R overload? How to document in Roxygen? (E.g., cores can be submitted as integer or character, and we can just convert to character when constructing command.)
+
+#' @description  Submit a job to Slurm.
+#'
+#' \code{submit_job()} Runs a script with sbatch, accepting standard sbatch arguments in addition to an optional list of arguments to pass to the script.
+#'
+#' @param script_path [character] Full path to the script. Argument for `sbatch -s`.
+#' @param job_name [character] Optional name to give job. Will be name of script if NULL.
+#' @param mem [character] Memory to allocate. Should be valid argument format for `sbatch --mem`.
+#' @param archiveTF [logical] To allow access to archive. Argument for `sbatch -C`.
+#' @param threads [character] Cores to allocate. Argument for `sbatch -c`.
+#' @param runtime [character] Minutes of runtime to allow. Argument for `sbatch -t`.
+#' @param Partition [character] Partition to run on. Argument for `sbatch -p`.
+#' @param Account [character] Account to attribute job to.
+#' @param args_list [list()] Optional named list of arguments to pass to script, e.g. `list("--arg1" = arg1, "--arg2" = arg2)`.
+#' @param SLURM_ERROR_PATH [character] Optional filepath to slurm error output. If not NULL, will send errors and output to same log file. Argument for `sbatch -o`.
+#' @param R_IMAGE_PATH [character] Optional filepath to R image. Argument for `sbatch -i`.
+#' @param r_shell [character] Optional filepath to image shell script. No associated flag for `sbatch`.
+#' 
+#' @return [character] `submission_return`, the return message from submitting `sbatch` `command` to `system(command, intern = T)`.
 submit_job <- function(
-    script_path, # full path to the script
-    job_name = NULL, # Will be name of script if NULL.
-    mem = "10G", # memory
-    archiveTF = F, # Expects T/F
-    threads = "2", # threads (uge: fthread)
-    runtime = "20", # runtime, min
-    Partition = "d.q", # partition (uge: queue)
-    Account = "proj_covid", # account (uge: project)
-    args_list = NULL, # optional list() of arguments, e.g. `list("--arg1" = arg1, "--arg2" = arg2)`
+    script_path,
+    job_name = NULL,
+    mem = "10G", # TODO: validate for G (or other options for --mem?)
+    archiveTF = F,
+    threads = "2",
+    runtime = "20",
+    Partition = "d.q",
+    Account = "proj_covid",
+    args_list = NULL,
     SLURM_ERROR_PATH = NULL,
     R_IMAGE_PATH = NULL,
     r_shell = NULL
 ) {
+  
+  # TODO: Validate input.
   
   if (is.null(job_name)) {
     tmp <- unlist(strsplit(script_path, "[/.]"))
@@ -36,7 +58,11 @@ submit_job <- function(
     " -t ", runtime,
     " -p ", Partition,
     " -A ", Account,
+    # TODO: Only add if not NULL.
+    # TODO: Allow users to pass -e and -o, so that output and errors can be separate if they wish.
     " -o ", file.path(SLURM_ERROR_PATH, paste0(job_name, ".o%A_%a")),
+    # TODO: Only add if not NULL.
+    # TODO: Does image require shell argument.
     " ", r_shell,
     " -s ", script_path
   )
