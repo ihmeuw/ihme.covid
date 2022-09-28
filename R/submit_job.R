4ef8fed543f8f8edb1bd5d44d334147013ad6313
Kaleb Coberly <kalcob43@gen-slurm-slogin-p02.cluster.ihme.washington.edu>
2022-09-28 16:01:19 -0700

    Correct docstring.

--- R/submit_job.R
+++ R/submit_job.R
@@ -15,7 +15,7 @@
 #' @param partition [character] Partition to run on. Argument for `sbatch -p`.
 #' @param account [character] Account to attribute job to. Argument for `sbatch -A`.
 #' @param output_path [character] Optional filepath to slurm output. If NULL, will default to `file.path("/mnt/share/temp/slurmoutput", Sys.info()["user"], "errors")`. Will send errors and output to same log file if error_path is NULL. Argument for `sbatch -o`.
-#' @param error_path [character] Optional filepath to slurm error output. Will send errors and output to same log file. If NULL, will send errors to same log file as output. Argument for `sbatch -e`.
+#' @param error_path [character] Optional filepath to slurm error output. If NULL, will send errors to same log file as output. Argument for `sbatch -e`.
 #' @param sbatch_args_list [list()] Optional named list of arguments to pass to `sbatch`, e.g. `list("--arg1" = arg1, "--arg2" = arg2)`.
 #' @param shell_path [character] Optional filepath to image shell script. If NULL, defaults to `/ihme/singularity-images/rstudio/shells/jpy_rstudio_sbatch_script.sh`.
 #' @param image_path [character] Optional filepath to image. Argument for `shell_path -i`.
